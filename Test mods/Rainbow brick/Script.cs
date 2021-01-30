c00bcf05140b214d3ebc0ae6337af2a62db42e12
morzzic <65243242+morzzic@users.noreply.github.com>
2021-01-30 23:32:55 +0300

    Add files via upload

---
+++ Test mods/Rainbow brick/Script.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class RainbowBrickMod : MonoBehaviour
+{
+    public static void Main()
+    {
+        Modification Modification = new Modification("Rainbow brick", "Brick");
+        Modification.Data.CategoryName = "Props";
+        Modification.Data.Description = "It changes colors.";
+        Modification.Data.Thumbnail = Modification.LoadSprite("None.png");
+		
+        Modification.Data.SpawnAction += (Instance) =>
+		{
+			Instance.AddComponent<RainbowBrick>();
+        };
+    }
+}
+
+public class RainbowBrick : MonoBehaviour
+{
+	SpriteRenderer sprite;
+	
+	void Awake()
+	{
+		sprite = GetComponent<SpriteRenderer>();
+		InvokeRepeating("Change", 0, 0.5f);
+	}
+	
+	void Change()
+	{
+		sprite.color = UnityEngine.Random.ColorHSV();
+	}
+}
