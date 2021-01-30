fffe0b2f2b482aefb89d777c3d9d04e422c4acf5
morzzic <65243242+morzzic@users.noreply.github.com>
2021-01-30 22:57:08 +0300

    Add files via upload

---
+++ Empty mod/Script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EmptyMod : MonoBehaviour
+{
+    public static void Main()
+    {
+        Modification Modification = new Modification("Empty item", "Brick"); //Set name and original item
+        Modification.Data.CategoryName = "Props"; //Set category
+        Modification.Data.Description = "Description"; //Set description
+        Modification.Data.Thumbnail = Modification.LoadSprite("None.png"); //Set thumbnail
+		
+		//On spawn
+        Modification.Data.SpawnAction += (Instance) =>
+		{
+			Debug.Log("Spawn!");
+        };
+    }
+}
